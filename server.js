dd0591504b558f1e0f00aed69bd778c13f971145
Michael Colby <michael@Michaels-MacBook-Pro.local>
2019-05-29 06:30:17 -0500

    Trying using only port 80

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const cors = require('cors')
 const spotify = require('./spotify-services')
 const app = express()
-const port = process.env.PORT
+const port = 80
 
 var token = null;
 var code = null;
